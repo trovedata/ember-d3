caff73123698aee0b3f8485fdbc7582cc29dfd26
ricardo-beas <rbeas@trovedata.com>
2017-05-16 13:31:15 -0400

    Vendor fixes

--- index.js
+++ index.js
@@ -16,7 +16,7 @@ module.exports = {
       // In ember-cli < 2.7, this.import is not available, so fall back to use app.import
       var importShim = typeof this.import !== 'undefined' ? this : app;
 
-      app.import(path.join(d3Path, 'd3.js'));
+      importShim.import(path.join(d3Path, 'd3.js'));
       importShim.import('vendor/ember-d3/ember-d3-shim.js', {
         exports: {
           d3: ['default']
@@ -25,17 +25,19 @@ module.exports = {
     }
   },
 
-  treeForVendor: function() {
+  treeForVendor: function(vendorTree) {
     var trees = [];
-    var highchartsPath = path.dirname(require.resolve('d3'));
+    var d3Path = path.dirname(require.resolve('d3'));
 
     if (vendorTree) {
       trees.push(vendorTree);
     }
 
-    trees.push(new Funnel(vendorTree, {
+    trees.push(new Funnel(d3Path, {
       destDir: 'd3'
     }));
+
+    trees.push(path.join(__dirname, 'client'));
 
     return mergeTrees(trees);
   }
