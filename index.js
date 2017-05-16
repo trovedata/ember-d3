364542c506b48bf10448bdf0ea53c7405df96d99
ricardo-beas <rbeas@trovedata.com>
2017-05-16 12:41:34 -0400

    Funnel Dependency and node import

--- index.js
+++ index.js
@@ -1,6 +1,8 @@
 /* jshint node: true */
 'use strict';
 
+var Funnel = require('broccoli-funnel');
+var mergeTrees = require('broccoli-merge-trees');
 var path = require('path');
 
 module.exports = {
@@ -10,11 +12,11 @@ module.exports = {
     this._super.included(app);
     if (process.env.EMBER_CLI_FASTBOOT !== 'true') {
       // In nested addons, app.bowerDirectory might not be available
-      var bowerDirectory = app.bowerDirectory || 'bower_components';
+      var d3Path = 'vendor/d3';
       // In ember-cli < 2.7, this.import is not available, so fall back to use app.import
       var importShim = typeof this.import !== 'undefined' ? this : app;
 
-      importShim.import(bowerDirectory + '/d3/d3.js');
+      app.import(path.join(d3Path, 'd3.js'));
       importShim.import('vendor/ember-d3/ember-d3-shim.js', {
         exports: {
           d3: ['default']
@@ -24,6 +26,17 @@ module.exports = {
   },
 
   treeForVendor: function() {
-    return path.join(__dirname, 'client');
+    var trees = [];
+    var highchartsPath = path.dirname(require.resolve('d3'));
+
+    if (vendorTree) {
+      trees.push(vendorTree);
+    }
+
+    trees.push(new Funnel(vendorTree, {
+      destDir: 'd3'
+    }));
+
+    return mergeTrees(trees);
   }
 };
